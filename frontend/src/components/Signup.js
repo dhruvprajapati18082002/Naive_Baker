8759405eaa03c1c79a8fa585b3e9988548dcf7a1
techNik11 <nikhiljj2002@gmail.com>
2023-04-04 01:34:21 +0530

    Modification to signup page because of the css issue

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,61 +1,38 @@
-import React, { useState } from 'react';
-import '../signup.css';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
+import './login.css'
 
-function Signup() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+export default function Signup() {
 
-  function handleEmailChange(event) {
-    setEmail(event.target.value);
-  }
+    return (
+        <div className="text-center m-5-auto">
+            <h2>Join us</h2>
+            <h5>Create your personal account</h5>
+            <form action="/home">
+                <p>
+                    <label>Username</label><br/>
+                    <input type="text" name="first_name" required />
+                </p>
+                <p>
+                    <label>Email address</label><br/>
+                    <input type="email" name="email" required />
+                </p>
+                <p>
+                    <label>Password</label><br/>
+                    <input type="password" name="password" requiredc />
+                </p>
+                <p>
+                    <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
+                </p>
+                <p>
+                    <button id="sub_btn" type="submit">Register</button>
+                </p>
+            </form>
+            <footer>
+                <p><Link to="/">Back to Homepage</Link>.</p>
+            </footer>
+        </div>
+    )
 
-  function handlePasswordChange(event) {
-    setPassword(event.target.value);
-  }
-
-  function handleConfirmPasswordChange(event) {
-    setConfirmPassword(event.target.value);
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    // Add code to handle signup logic here
-  }
-  return (
-    <div className="container">
-      <form className="form-container" onSubmit={handleSubmit}>
-        <h2>Sign Up</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          className="form-input"
-          value={email}
-          onChange={handleEmailChange}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          className="form-input"
-          value={password}
-          onChange={handlePasswordChange}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Confirm Password"
-          className="form-input"
-          value={confirmPassword}
-          onChange={handleConfirmPasswordChange}
-          required
-        />
-        <button type="submit" className="submit-btn">Sign Up</button>
-        <p className="signup-link">Already have an account? <a href="/login">Log in</a></p>
-      </form>
-    </div>
-  ); 
-}   
-
-export default Signup;
+}
