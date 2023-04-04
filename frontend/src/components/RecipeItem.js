f12859a6cac512eb0c805222d0f834a8cd3d4dc5
Aditya <adityasraj123@gmail.com>
2023-04-05 00:22:05 +0530

    Search implemented

--- frontend/src/components/RecipeItem.js
+++ frontend/src/components/RecipeItem.js
@@ -1,21 +1,13 @@
 import React from 'react'
-
+import Search from './Search'
 export default function RecipeItem(props) {
     return (
-        <div className=" card">
-            <div className="card text-center">
-                <div className="card-header ">
-                    {props.header}
-                </div>
-                <div className="card-body">
-                    <h5 className="card-title">{props.title}</h5>
-                    <p className="card-text">{props.text}</p>
-                    <a href="#" className="btn btn-sm btn-primary">{props.btnText}</a>
-                </div>
+        <div className="empty">
             <div className="card-footer text-muted">
+            <Search />
                 {props.footer}
             </div>
-            </div>
         </div>
+
     )
 }
