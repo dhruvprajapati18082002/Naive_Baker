1e89dc32f519c6145d211ea8edc631bf0e3d80f3
Vihar <202001110@daiict.ac.in>
2023-04-07 00:10:15 +0530

    Updated Dev Details
    The Modified About Us page now fetches details from the developers.json file.
    The developers data is also updated to remove "age" and "description" and add "img" attributes

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -1,158 +1,88 @@
 import React from 'react'
+import developers from "../developers.json";
 
 export default function AboutUs() {
     return (
-    <><div class="bg-light">
-            <div class="container py-5">
-                <div class="row h-100 align-items-center py-5">
-                    <div class="col-lg-6">
-                        <h1 class="display-4">About us:</h1>
-                        <p class="lead text-muted mb-0">We work here to make your cooking experience memorable. </p>
-                        
+    <>
+        <div className="bg-light">
+            <div className="container py-5">
+                <div className="row h-100 align-items-center py-5">
 
+                    <div className="col-lg-6">
+                        <h1 className="display-4">About us:</h1>
+                        <p className="lead text-muted mb-0">We work here to make your cooking experience memorable. </p>
                     </div>
-                    <div class="col-lg-6 d-none d-lg-block"><img src="https://www.pngall.com/wp-content/uploads/12/Cooking-PNG-Free-Image.png" alt="" class="img-fluid" /></div>
+
+                    <div className="col-lg-6 d-none d-lg-block">
+                        <img src="https://www.pngall.com/wp-content/uploads/12/Cooking-PNG-Free-Image.png" alt="" className="img-fluid" />
+                    </div>
+
                 </div>
             </div>
-        </div><div class="bg-white py-5">
-                <div class="container py-5">
-                    <div class="row align-items-center mb-5">
-                        <div class="col-lg-6 order-2 order-lg-1"><i class="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
-                            <h2 class="font-weight-light">Perks of being a member</h2>
-                            <p class="font-italic text-muted mb-4">On being a member of 'Naive Baker', you can have your personalized dashboard where they can upload your own recipes and even save different recipes. Enjoy cooking!!</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
-                        </div>
-                        <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://static.vecteezy.com/system/resources/thumbnails/021/617/170/small/member-card-3d-render-icon-illustration-png.png" alt="" class="img-fluid mb-4 mb-lg-0" /></div>
+        </div>
+        
+        <div className="bg-white py-5">
+            <div className="container py-5">
+                <div className="row align-items-center mb-5">
+        
+                    <div className="col-lg-6 order-2 order-lg-1"><i className="fa fa-bar-chart fa-2x mb-3 text-primary"></i>
+                        <h2 className="font-weight-light">Perks of being a member</h2>
+                        <p className="font-italic text-muted mb-4">On being a member of 'Naive Baker', you can have your personalized dashboard where they can upload your own recipes and even save different recipes. Enjoy cooking!!</p><a href="#" className="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
                     </div>
-                    <div class="row align-items-center">
-                        <div class="col-lg-5 px-5 mx-auto"><img src="https://www.pngarts.com/files/8/Mobile-App-PNG-Image-Transparent-Background.png" alt="" class="img-fluid mb-4 mb-lg-0" /></div>
-                        <div class="col-lg-6"><i class="fa fa-leaf fa-2x mb-3 text-primary"></i>
-                            <h2 class="font-weight-light">Future Plans</h2>
-                            <p class="font-italic text-muted mb-4">We are planning to make a mobile application to make Naive Baker reach everyone and everywhere.</p><a href="#" class="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
+        
+                    <div className="col-lg-5 px-5 mx-auto order-1 order-lg-2">
+                        <img src="https://static.vecteezy.com/system/resources/thumbnails/021/617/170/small/member-card-3d-render-icon-illustration-png.png" alt="" className="img-fluid mb-4 mb-lg-0" /></div>
+                    </div>
+
+                    <div className="row align-items-center">
+                        <div className="col-lg-5 px-5 mx-auto">
+                            <img src="https://www.pngarts.com/files/8/Mobile-App-PNG-Image-Transparent-Background.png" alt="" className="img-fluid mb-4 mb-lg-0" />
+                        </div>
+                        <div className="col-lg-6"><i className="fa fa-leaf fa-2x mb-3 text-primary"></i>
+                            <h2 className="font-weight-light">Future Plans</h2>
+                            <p className="font-italic text-muted mb-4">We are planning to make a mobile application to make Naive Baker reach everyone and everywhere.</p><a href="#" className="btn btn-light px-5 rounded-pill shadow-sm">Learn More</a>
                         </div>
                     </div>
+
                 </div>
-            </div><div class="bg-light py-5">
-                <div class="container py-5">
-                    <div class="row mb-4">
-                        <div class="col-lg-5">
-                            <h2 class="display-4 font-weight-light">Our team</h2>
-                            <p class="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+            
+            <div className="bg-light py-5">
+                <div className="container py-5">
+                    <div className="row mb-4">
+                        <div className="col-lg-5">
+                            <h2 className="display-4 font-weight-light">Our team</h2>
+                            <p className="font-italic text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
                         </div>
                     </div>
 
-                    <div class="row text-center">
+                    <div className="row text-center">
 
-                    <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Vihar Shah</h5><span>Developer - Frontend and Backend</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Nikhil J Jethanandani</h5><span>Developer - Frontend</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                            
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Gaurav Shah</h5><span>Developer - Frontend</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Dhruv Prajapati</h5><span>Developer - Backend</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-1.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Aditya Raj</h5><span>Developer - Frontend</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Deep Rakhasiya</h5><span>Developer</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Priyank Pitiliya</h5><span>Developer</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Shobit Verma</h5><span>Developer</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-3 col-sm-6 mb-5">
-                            <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-2.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
-                                <h5 class="mb-0">Vinal Boricha</h5><span>Developer</span>
-                                <ul class="social mb-0 list-inline mt-3">
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-                                    <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-
+                        {
+                        developers.map((dev, key) =>{
+                            return (
+                                <div className="col-xl-3 col-sm-6 mb-5" key={key}>
+                                    <div className="bg-white rounded shadow-sm py-5 px-4">
+                                        <img src={dev.img} width="100" className="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm" />
+                                        <h5 className="mb-0">{dev.name}</h5>
+                                        {
+                                            dev.roles.map((role, key)=>{
+                                                return (
+                                                    <div key={key}>{role}</div>
+                                                )
+                                            })
+                                        }
+                                        <ul className="social mb-0 list-inline mt-3">
+                                            <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-facebook-f"></i></a></li>
+                                            <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-twitter"></i></a></li>
+                                            <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-instagram"></i></a></li>
+                                            <li className="list-inline-item"><a href="#" className="social-link"><i className="fa fa-linkedin"></i></a></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            )
+                        })
+                        }
                     </div>
                 </div>
                 <div className="card">
