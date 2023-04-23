cc09211136de2ddbc181ed6c72829e7097b221a9
ShobhitV15 <123548201+ShobhitV15@users.noreply.github.com>
2023-04-23 16:15:59 +0530

    added social media links in footer
    Instagram, twitter, and facebook page links added in the footer

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -37,17 +37,17 @@ export default function Footer() {
                     </p>
 
                     <ul className="list-unstyled d-flex">
-                        <li className="ms-3"><Link className="instagram" to="#">
+                        <li className="ms-3"><Link className="instagram" target="_blank" to="https://instagram.com/naive_baker?igshid=ZDdkNTZiNTM=">
                             <h6 className="mb-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -1 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-instagram mr-2 icon-inline text-danger"><rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect><path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path><line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
                                 </svg></h6>
                         </Link></li>
-                        <li className="ms-3"><Link className="twitter" to="#">
+                        <li className="ms-3"><Link className="twitter" target="_blank" to="https://twitter.com/BakerNaive82783">
                             <h6 className="mb-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -1 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-twitter mr-2 icon-inline text-info"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
                                 </svg></h6>
                         </Link></li>
-                        <li className="ms-3"><Link className="facebook" to="#">
+                        <li className="ms-3"><Link className="facebook" target="_blank" to="https://www.facebook.com/people/Naive-Baker/pfbid04UCGGpbDMp1Nt9pKuXNJLLN7PLEomFSjMAnikYuM8ShxiEpGj1g4hxEySVYg9ps4l/">
                             <h6 className="mb-0">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 -1 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-facebook mr-2 icon-inline text-primary"><path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
                                 </svg></h6>
