b48d8154098a1b6b080b296d2a7ea85dfe21249e
Gaurav Shah <shahg152002@gmail.com>
2023-04-25 00:30:07 +0530

    added ingrediant section
    added ingrediant section with a tag functionality through which you can added multiple ingrediant tags

--- frontend/src/components/dashboardComponents/UploadRecipe.js
+++ frontend/src/components/dashboardComponents/UploadRecipe.js
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+// import './boxstyle.css'
 
 export default function UploadRecipe() {
+    const onSubmit = (e) => {
+        e.preventDefault();
+        console.log("refresh prevented");
+      };
+
+    const [tags, setTags] = useState([]);
+    const addTag = (e) => {
+      if (e.key === "Enter") {
+        if (e.target.value.length > 0) {
+          setTags([...tags, e.target.value]);
+          e.target.value = "";
+        }
+      }
+    };
+    const removeTag = (removedTag) => {
+      const newTags = tags.filter((tag) => tag !== removedTag);
+      setTags(newTags);
+    };
+
+
     return (
         <section className="h-100 h-custom" style={{backgroundColor: '#8fc4b7'}}>
             <div className="container py-5 h-100">
@@ -11,7 +31,7 @@ export default function UploadRecipe() {
                     <div className="card-body p-4 p-md-5">
                         <h3 className="mb-4 pb-2 pb-md-0 mb-md-5 px-md-2">Upload Recipe</h3>
 
-                        <form className="px-md-2">
+                        <form className="px-md-2" onSubmit={onSubmit}>
                             
                             {/* this is for name input */}
                             <div className="form-outline mb-4">
@@ -21,20 +41,33 @@ export default function UploadRecipe() {
                                     id="recipetitle"
                                     name="recipetitle"
                                     className="form-control"
+                                    required
+                                    minLength={4}
+                                    maxLength={17}
                                 />
                             </div>
 
                             {/* this is for description */}
                             <div className="mb-3">
-                                <label for="exampleFormControlTextarea1" className="form-label">Description of Recipe*</label>
-                                <textarea className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                                <label htmlFor="exampleFormControlTextarea1" className="form-label">Description of Recipe*</label>
+                                <textarea className="form-control" 
+                                id="exampleFormControlTextarea1" 
+                                rows="3" 
+                                required 
+                                minLength={10} 
+                                maxLength={50} />
                             </div>
 
 
                             {/* this is for cuisine */}
                             <div className="form-outline mb-4">
-                                <label for="cuisine" className="form-label">Enter Cuisine*</label>
-                                    <input className="form-control" list="types" id="cuisine" placeholder="Type to search..."/>
+                                <label htmlFor="cuisine" className="form-label">Enter Cuisine*</label>
+                                    <input className="form-control" 
+                                    list="types" 
+                                    id="cuisine" 
+                                    placeholder="Type to search..."
+                                    required
+                                    />
                                     <datalist id="types">
                                         <option value="Indian" />
                                         <option value="French" />
@@ -48,8 +81,13 @@ export default function UploadRecipe() {
                             
                             {/* this is for veg/non-veg */}
                             <div className="form-outline mb-4">
-                                <label for="cuisinetype" className="form-label">Cuisine type*</label>
-                                    <input className="form-control" list="cuisineoptions" id="cuisinetype" placeholder="Type to search...veg/non-veg/vegan"/>
+                                <label htmlFor="cuisinetype" className="form-label">Cuisine type*</label>
+                                    <input className="form-control" 
+                                    list="cuisineoptions" 
+                                    id="cuisinetype" 
+                                    placeholder="Type to search...veg/non-veg/vegan"
+                                    required
+                                    />
                                         <datalist id="cuisineoptions">
                                             <option value="Vegetarian" />
                                             <option value="Non Vegetarian" />
@@ -58,17 +96,41 @@ export default function UploadRecipe() {
                             </div>
                             {/* this is duration */}
                             <div className="form-outline mb-4">
-                                <label for="recipeduration" className="form-label">Duration*</label>
-                                    <input className="form-control" type="number" id="recipeduration" placeholder="in minutes.."/>
+                                <label htmlFor="recipeduration" className="form-label">Duration*</label>
+                                    <input className="form-control" 
+                                    type="number" 
+                                    id="recipeduration" 
+                                    placeholder="in minutes.."
+                                    required
+                                    />
                             </div>
 
                             {/* this is for ingrediants */}
+                            <div className="form-outline mb-4">
+                            <label htmlFor="boxes">Ingrediants*</label>
+                                <div className="form-control" id="boxes" >
+                                    {tags.map((tag, index) => {
+                                    return (
+                                        <div key={index} >
 
+                                        {tag} <span onClick={() => removeTag(tag)} >x</span>
+                                        </div>
+                                    );
+                                    })}
+                                    
+                                    <input onKeyDown={addTag} />
+                                </div>
+                                </div>
+  
 
                             {/* this is for steps of recipe */}
                             <div className="mb-3">
-                                <label for="recipetutorial" className="form-label">Step by step tutorial*</label>
-                                <textarea className="form-control" id="recipetutorial" rows="3" placeholder='begin new step from new line...'></textarea>
+                                <label htmlFor="recipetutorial" className="form-label">Step by step tutorial*</label>
+                                <textarea className="form-control" 
+                                id="recipetutorial" 
+                                rows="3" 
+                                placeholder='begin new step from new line...'
+                                required />
                             </div>
 
                             
